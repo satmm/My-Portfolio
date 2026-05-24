6540bc2523df74769f6ac30da3801140e69e86a1
satmm <satyamsingh.on@gmail.com>
2026-05-24 16:41:18 +0530

    Enhance about section: Update resume link, refine personal description, and add highlights for skills and expertise.

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,4 +52,4 @@ const sr = ScrollReveal({
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.experience__item, .skills__data, .work__img, .contact__input',{interval: 200}); 
+sr.reveal('.about__highlights span, .experience__item, .skills__category, .skills__data, .work__img, .contact__input',{interval: 200}); 
